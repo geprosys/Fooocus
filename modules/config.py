29f02138ec58343945875a5f7312b548ef873461
geprosys <154334949+geprosys@users.noreply.github.com>
2023-12-20 11:15:16 +0100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -170,8 +170,8 @@ default_loras = get_config_item_or_set_default(
     key='default_loras',
     default_value=[
         [
-            "sd_xl_offset_example-lora_1.0.safetensors",
-            0.1
+            "LCMTurboMix_DPM_SDE_Karras.safetensorss",
+            1.0
         ],
         [
             "None",
